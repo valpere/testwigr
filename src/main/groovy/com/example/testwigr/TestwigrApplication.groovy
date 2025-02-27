75a3095b9bc7f9d79214ff997d34d0ed540ad9dc
Valentyn Solomko <valentyn.solomko@gmail.com>
2025-02-27 17:38:39 +0200

    Generate the project

---
+++ src/main/groovy/com/example/testwigr/TestwigrApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.testwigr
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class TestwigrApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(TestwigrApplication, args)
+	}
+
+}
---
+++ src/test/groovy/com/example/testwigr/TestwigrApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.example.testwigr
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TestwigrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
